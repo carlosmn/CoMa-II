736a1ff8e69a92fe527ecf0151e3453b0205586c
Carlos Martín Nieto <cmn@dwim.me>
2013-06-30 20:57:35 +0200

    Z10: pretty-print the graphs

--- u-10/HP.m
+++ u-10/HP.m
@@ -6,10 +6,10 @@ function HP
   first = res(1,:);
   second = res(2,:);
   hold off
-  %subplot(2,1,1)
-  %plot(0:0.1:10, first, 'b')
-  %hold on
-  %plot(0:0.1:10, second, 'r')
+  subplot(3,2, [1 2])
+  plot(0:0.1:10, first, 'b')
+  hold on
+  plot(0:0.1:10, second, 'r')
 
   dres = [];
   for i=1:length(res)
@@ -17,19 +17,26 @@ function HP
       dres = [dres d];
   end
 
-  %plot(0:0.1:10, sqrt(dres(1,:).^2 + dres(2,:).^2))
-  %subplot(2,1,2)
+  plot(0:0.1:10, sqrt(dres(1,:).^2 + dres(2,:).^2), 'g')
+
+  legend('x', 'y', 'E')
 
   N = 20;
-  n = newton(1, @(x) cos(x), @(x) -sin(x), -1, N)
+
+  subplot(3,2,3)
+  n = newton(3, @(x) cos(x), @(x) -sin(x), -1, N);
   plot(n, 'r')
+  legend('cos(x) = -1')
 
-  hold on
-  n = newton(1, @(x) atan(x), @(x) (1/(x^2 + 1)), pi/4.0, N)
+  subplot(3,2,4)
+  n = newton(1.1, @(x) atan(x), @(x) (1/(x^2 + 1)), pi/4.0, N);
   plot(n, 'b')
+  legend('arctan(x) = \pi/4')
 
-  n = newton(1, @(x) x^(1/3), @(x) (x^(-2/3))/3.0, 2, N)
+  subplot(3,2,5)
+  n = newton(8.5, @(x) x^(1/3), @(x) (x^(-2/3))/3.0, 2, N);
   plot(n, 'g')
+  legend('x^{1/3} = 2')
 end
 
 function xs = trapez(x0, tau, N, A)
@@ -39,7 +46,7 @@ function xs = trapez(x0, tau, N, A)
     T1 = E + (tau/2.0)*A;
     T2 = inv(E - (tau/2.0)*A);
     
-    x = xs(:,k) \ (T1 * T2);
+    x = xs(:,k) \ (T2 * T1);
 
     xin = [x(1); x(2)];
     xs = [xs xin];
